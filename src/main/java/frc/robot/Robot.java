23da582ea9636547fa87ab6b2a67a7328ad2f764
AvengerRobotics <admin@avengerrobotics.com>
2020-01-16 19:02:43 -0500

    winch + motor groups - dhruva, ian, kyle, karthik

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,21 +23,24 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive; //used for driving differe
 public class Robot extends TimedRobot { 
   private SpeedControllerGroup leftMotors; //Creates the object for the left motors
   private SpeedControllerGroup rightMotors; //Creates the object for the right motors
-  
+  private SpeedControllerGroup intakeMotors; //Creates the object for the intake motors
+  private SpeedControllerGroup winchMotors; //Creates the object for the winch motors
+  private WPI_VictorSPX WoFMotor; //Creates the object for the WoF motors
+  private WPI_VictorSPX liftMotor; //Creates the object for the lift motors
   private DifferentialDrive myRobot; //Creates the object for the drivetrain
   private Joystick controller; //Creates the object for the contoller
 
-  private WPI_VictorSPX intakeMotor; //Creates the object for the intake motor
+  //private WPI_VictorSPX intakeMotor; //Creates the object for the intake motor
   private JoystickButton aButton; //Creates a container to receive a boolean from the A Button
-  private JoystickButton bButton; //Creates a container to receive a boolean from the B Button
-  private int intakeMotorValue; //Creating a variable for the speed of the intake motor controller
-
-  private WPI_VictorSPX conveyerMotor; //creates the object for the conveyer
+  //private JoystickButton bButton; //Creates a container to receive a boolean from the B Button
+  private int intakeMotorsValue; //Creating a variable for the speed of the intake motor controller
+  private int liftMotorValue; //Creating a variable for the speed of the lift motor controller
+  //private WPI_VictorSPX conveyerMotor; //creates the object for the conveyer
   private JoystickButton buttonX; //Creates a container to receive a boolean from the X Button
   private JoystickButton buttonY; //Creates a container to receive a boolean from the Y Button
-  private int conveyerMotorValue;
-
-  private SpeedControllerGroup winchMotors;
+  //private int conveyerMotorValue;
+  private JoystickButton lbButton; //Creates a container to receive a boolean form the LB button
+  private JoystickButton rbButton; //Creates a container to receive a boolean form the RB button 
 
   @Override
   public void robotInit() {
@@ -45,53 +48,72 @@ public class Robot extends TimedRobot {
     Assigns the motor controllers to speed controller groups
     Argument(value in parenthises) is the CAN bus address
     */
-
     leftMotors = new SpeedControllerGroup(new WPI_VictorSPX(1), new WPI_VictorSPX(4));
     rightMotors = new SpeedControllerGroup(new WPI_VictorSPX(2), new WPI_VictorSPX(3));
+    intakeMotors = new SpeedControllerGroup(new WPI_VictorSPX(5), new WPI_VictorSPX(6));
+    winchMotors = new SpeedControllerGroup(new WPI_VictorSPX(9), new WPI_VictorSPX(10));
+    liftMotor = new WPI_VictorSPX(7);
+    WoFMotor = new WPI_VictorSPX(8);
+    
 
     myRobot = new DifferentialDrive(leftMotors, rightMotors);
     //Creates the controller on USB 0
     controller = new Joystick(0);
     
-    intakeMotor = new WPI_VictorSPX(5); //Assigns intake motor as CAN bus 5
+    //intakeMotor = new WPI_VictorSPX(5); //Assigns intake motor as CAN bus 5
     aButton = new JoystickButton(controller,1); //Assigns the aButton variable to the A button on Controller 0
-    bButton = new JoystickButton(controller,2); //Assigns the bButton variable to the B button on Controller 0
-
+    //bButton = new JoystickButton(controller,2); //Assigns the bButton variable to the B button on Controller 0
     // Assigns button X and Y to button 3 and 4 on the controller
     buttonX = new JoystickButton(controller, 3);
     buttonY = new JoystickButton(controller, 4);
-    conveyerMotor = new WPI_VictorSPX(6); //Assigns conveyer motor to CAN 6
-
-    winchMotors = new SpeedControllerGroup(new WPI_VictorSPX(9), new WPI_VictorSPX(10));
+    //conveyerMotor = new WPI_VictorSPX(6); //Assigns conveyer motor to CAN 6
+    lbButton = new JoystickButton(controller,5); //left bumper of the controller
+    rbButton = new JoystickButton(controller,6); //right bumper of the controller
   }
 
   @Override
   public void teleopPeriodic() {
     //Sets tankDrive to the inverse of the values from the joysticks, leftStick value is 1 and rightStick value is 5
     myRobot.tankDrive((controller.getRawAxis(1)*-1), (controller.getRawAxis(5)*-1));
+    controller.getPOV();
+    if(controller.getPOV() > 45 && controller.getPOV() < 135 == true){ 
+      WoFMotor.set(1);
+    //If the A button is pressed, then the conveyor and intake motors speed will be set to -1
+    } else if(controller.getPOV() > 225 && controller.getPOV() < 315 == true){ 
+      WoFMotor.set(-1);
+      //If neither A or Y button is pressed, then the conveyor and intake motors speed will be set to 0
+    } else{
+      WoFMotor.set(0);
+    }
+      //Sets the intake motors speed to the variable intakeMotorsValue
+    intakeMotors.set(intakeMotorsValue);
 
-    
-    //Converts a boolean to a 1 or 0 based on the A button state
-    if(aButton.get() == true) 
-      intakeMotorValue = 1;   
-    // Converts a boolean to a -1 or 0 based on the B button state
-    if(bButton.get() == true) 
-      intakeMotorValue = (-1);   //sets the motor to run in reverse when B is pressed
-    //The motor power will be sat to 0 when both A and B are not pressed
-    else if(aButton.get() == false && bButton.get() == false)
-      intakeMotorValue = 0;
-    //Sets the intake motor speed to the variable intakeMotorValue
-    intakeMotor.set(intakeMotorValue);
-
-
-    //Sets the motor value based on the value of buttonX and buttonY
-    if(buttonX.get() == true) 
-      conveyerMotorValue = 1;
-    if(buttonY.get() == true)
-      conveyerMotorValue = (-1);
-    else if(buttonX.get() == false && buttonY.get() == false)
-      conveyerMotorValue = 0;
-
-    conveyerMotor.set(conveyerMotorValue);
+    if (buttonX.get()){
+      winchMotors.set(-1*controller.getRawAxis(3));
+    } else {
+      winchMotors.set(controller.getRawAxis(3));
+    }
+  
+    //If the Y button is pressed, then the conveyor and intake motors speed will be set to 1
+    if(buttonY.get() == true){ 
+      intakeMotorsValue = 1;
+    //If the A button is pressed, then the conveyor and intake motors speed will be set to -1
+    } else if(aButton.get() == true){
+      intakeMotorsValue = (-1);
+      //If neither A or Y button is pressed, then the conveyor and intake motors speed will be set to 0
+    } else if(buttonY.get() == false && aButton.get() == false){
+      intakeMotorsValue = 0;
+    }
+    //Sets the intake motors speed to the variable intakeMotorsValue
+    intakeMotors.set(intakeMotorsValue);
+   
+    if(lbButton.get() == true){  //If the LB Button is pressed, then the lift motor speed will be set to 1
+      liftMotorValue = 1;
+    } else if(rbButton.get() == true){ //If the RB Button is pressed, then the lift motor speed will be set to -1
+      liftMotorValue = -1;
+    } else if(lbButton.get() == false && rbButton.get() == false){  //If neither LB or RB button is pressed, then the lift motor speed will be set to 0
+      liftMotorValue = 0;
+    }
+    liftMotor.set(liftMotorValue); //Sets the lift motor speed to the variable liftMotorValue
   }
 }
